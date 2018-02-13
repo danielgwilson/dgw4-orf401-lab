cb0007eac1dde377a23ee66138ced25ad9fac28f
danielgwilson <daniel@danielgwilson.com>
2018-02-12 23:37:45 -0500

    Templating 1, still no logo

--- app.py
+++ app.py
@@ -3,24 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '''
-        <html>
-            <title>ORF 401: Assignment #1 - Python - Spring 2018</title>
-            <body>
-                <center>
-                <br><br>
-                <h1>riidr carsharing service</h1>
-                <br><br>
-                <img src="riidr_logo.png"/>
-                <br><br>
-
-                <p>Enter a single origin/destination to search for:</p>
-
-                <form method="get">
-                    <input type="text" name="query"/>
-                    <input type="submit"/>
-                </form>
-                </center>
-            </body>
-        </html>
-    '''
+    return render_template('index.html', title='ORF 401: Assignment #1 - Python')
