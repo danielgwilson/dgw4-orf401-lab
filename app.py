4f7b85404658bc8da3411e3e12c357f5a96db9ad
danielgwilson <daniel@danielgwilson.com>
2018-02-13 02:30:14 -0500

    finished functional search

--- app.py
+++ app.py
@@ -1,16 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_material import Material
 from flask_wtf import Form
-from wtforms import TextField
+from wtforms import TextField, SubmitField
+from wtforms.validators import Required
+import sys
 
 app = Flask(__name__)
 Material(app)
 
+
 class ExampleForm(Form):
-    field1 = TextField('Search Field', description='Enter an origin or a destination')
-    submit_button = SubmitField('Submit Form')
+    search = TextField(
+        'Search Field', description='Enter an origin or a destination')
+
+
+def search_dat(file_name, search_term):
+    rides = []
+
+    with open(file_name, "r") as file:
+        for line in file:
+            name, source, dest, date, time, multiple, num = line.split(
+                "\t")
+
+            if search_term in source or search_term in dest:
+                rides.append(name + " " + source + " " + dest + " " + date + \
+                    " " + time + " " + multiple + " " + num)
+
+    return rides
+
 
 @app.route('/')
-def hello_world():
-    form = ExampleForm()
-    return render_template('index.html', title='ORF 401: Lab 1 - Python', form = form)
+def index():
+    form = ExampleForm(csrf_enabled=False)
+    if request.args:
+        query = request.args['search']
+        results = search_dat('riders.dat', query)
+        return render_template('index.html', title='ORF 401: Lab 1 - Python', form=form, results=results)
+    return render_template('index.html', title='ORF 401: Lab 1 - Python', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
