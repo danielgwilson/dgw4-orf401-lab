10ab6f1af93864fbdd66ad4a5644d9ab9b7b4624
danielgwilson <daniel@danielgwilson.com>
2018-02-13 01:43:37 -0500

    updates

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from wtforms import TextField
 app = Flask(__name__)
 Material(app)
 
+class ExampleForm(Form):
+    field1 = TextField('Search Field', description='Enter an origin or a destination')
+
 @app.route('/')
 def hello_world():
     return render_template('index.html', title='ORF 401: Lab 1 - Python')
