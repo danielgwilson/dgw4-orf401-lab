03abaffce878465be6ab26756a02293ece0a662e
danielgwilson <daniel@danielgwilson.com>
2018-02-12 23:24:46 -0500

    First crack at form

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '''
+        <html>
+            <title>ORF 401: Assignment #1 - Python - Spring 2018</title>
+            <body>
+                <br><br>
+                <h1 style="center">riidr carsharing service</h1>
+                <br><br>
+                <img src="riidr_logo.png"/>
+                <br><br>
+
+                <p>Enter a single origin/destination to search for:</p>
+
+                <form method="get">
+                    <input type="text" name="query"/>
+                    <input type="submit"/>
+                </form>
+            </body>
+        </html>
+    '''
