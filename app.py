30ac2779e19b10fb58def18d31d643f593f48ea1
danielgwilson <daniel@danielgwilson.com>
2018-02-13 01:01:51 -0500

    try to add material design

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from flask_material import Material
+from flask_wtf import Form
+from wtforms import TextField
 
 app = Flask(__name__)
+Material(app)
 
 @app.route('/')
 def hello_world():
