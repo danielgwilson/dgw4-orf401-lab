b465d95c340b84d342074e01e2a6b54f4bb27d72
danielgwilson <daniel@danielgwilson.com>
2018-02-14 00:04:45 -0500

    new data set and pandas

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_wtf import Form
 from wtforms import TextField, SubmitField
 from wtforms.validators import Required
 import sys
+import pandas as pd
 #from config import Config
 
 app = Flask(__name__)
@@ -20,14 +21,21 @@ class ExampleForm(Form):
 def search_dat(file_name, search_term):
     rides = []
 
-    with open(file_name, "r") as file:
-        for line in file:
-            name, source, dest, date, time, multiple, num = line.split(
-                "\t")
+    file = pd.read_csv(file_name)
 
-            if search_term in source or search_term in dest:
-                rides.append(name + " " + source + " " + dest + " " + date + \
-                    " " + time + " " + multiple + " " + num)
+    for row in file[file['City'].str.contains(search_term)].values.tolist():
+        rides.append(' '.join(str(s) for s in row))
+
+    # print(rides)
+    # with open(file_name, "r") as file:
+    #     reader = csv.reader(file)
+    #     for row in reader:
+    #         name, source, dest, date, time, multiple, num = line.split(
+    #             "\t")
+    #
+    #         if search_term in source or search_term in dest:
+    #             rides.append(name + " " + source + " " + dest + " " + date + \
+    #                 " " + time + " " + multiple + " " + num)
 
     return rides
 
@@ -37,7 +45,7 @@ def index():
     form = ExampleForm(csrf_enabled = False)
     if request.args:
         query = request.args['search']
-        results = search_dat('riders.dat', query)
+        results = search_dat('met_gala_attendees.csv', query)
         return render_template('index.html',
                                 title='ORF 401: Lab 1 - Python',
                                 form=form,
