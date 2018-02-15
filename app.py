0a6a1e5f874d252d9c56cd42a32834107f2e0686
danielgwilson <daniel@danielgwilson.com>
2018-02-14 21:29:15 -0500

    Updated styling, divs, and added table styling

--- app.py
+++ app.py
@@ -5,41 +5,40 @@ from wtforms import TextField, SubmitField
 from wtforms.validators import Required
 import sys
 import pandas as pd
-#from config import Config
 
 app = Flask(__name__)
-# app.config.from_object(Config)
 Material(app)
 
 
 class ExampleForm(Form):
     search = TextField(
-        'Search Field',
-        description='Enter an origin or a destination')
+        'Search Field', description='Origin or Destination')
 
 
 def search_dat(file_name, search_term):
-    rides = []
+    # rides = []
 
     file = pd.read_csv(file_name)
 
-    for row in file[file['City'].str.contains(search_term, case=False)].values.tolist():
-        rides.append(' '.join(str(s) for s in row))
+    # for row in file[file['City'].str.contains(search_term, case=False)].values.tolist():
+    #     rides.append(' '.join(str(s) for s in row))
 
-    return rides
+    return file[file['City'].str.contains(search_term, case=False)].values.tolist()
 
 
 @app.route('/')
 def index():
     form = ExampleForm(csrf_enabled = False)
     if request.args:
-        query = request.args['search']
-        results = search_dat('met_gala_attendees.csv', query)
-        return render_template('index.html',
-                                title='ORF 401: Lab 1 - Python',
-                                form=form,
-                                results=results)
-    return render_template('index.html', title='ORF 401: Lab 1 - Python', form=form)
+        if request.args['search'] != '':
+            query = request.args['search']
+            results = search_dat('met_gala_attendees.csv', query)
+            return render_template('index_mdl.html',
+                                    title='ORF 401: Lab 1 - Python',
+                                    form=form,
+                                    results=results)
+
+    return render_template('index_mdl.html', title='ORF 401: Lab 1 - Python', form=form)
 
 
 if __name__ == '__main__':
