d558ac73c786109203c88458a2fb48a83d9f520c
danielgwilson <daniel@danielgwilson.com>
2018-02-14 23:43:12 -0500

    Added chips, card for Oscars, adjusted margins and logo size.

--- app.py
+++ app.py
@@ -16,29 +16,40 @@ class ExampleForm(Form):
 
 
 def search_dat(file_name, search_term):
-    # rides = []
+    # search the dataset for a query
 
-    file = pd.read_csv(file_name)
+    df = pd.read_csv(file_name)
+    return df[df['Address'].str.contains(search_term, case=False) |
+                df['City'].str.contains(search_term, case=False) |
+                df['State'].str.contains(search_term, case=False)].values.tolist()
 
-    # for row in file[file['City'].str.contains(search_term, case=False)].values.tolist():
-    #     rides.append(' '.join(str(s) for s in row))
-
-    return file[file['City'].str.contains(search_term, case=False)].values.tolist()
+def get_top_origins(file_name):
+    df = pd.read_csv(file_name)
+    return df['City'].value_counts().index.values.tolist()[0:4]
 
 
 @app.route('/')
 def index():
+    data_file_name = 'met_gala_attendees.csv'
     form = ExampleForm(csrf_enabled = False)
+
+    # if there was a query entered, build a table
+    query = ''
+    results = None
     if request.args:
         if request.args['search'] != '':
             query = request.args['search']
-            results = search_dat('met_gala_attendees.csv', query)
-            return render_template('index_mdl.html',
-                                    title='ORF 401: Lab 1 - Python',
-                                    form=form,
-                                    results=results)
+            results = search_dat(data_file_name, query)
 
-    return render_template('index_mdl.html', title='ORF 401: Lab 1 - Python', form=form)
+    # make chips out of the top 4 cities
+    chips = get_top_origins(data_file_name)
+
+    return render_template('index_mdl.html',
+                            title='ORF 401: Lab 1 - Python',
+                            form=form,
+                            query=query,
+                            results=results,
+                            chips=chips)
 
 
 if __name__ == '__main__':
